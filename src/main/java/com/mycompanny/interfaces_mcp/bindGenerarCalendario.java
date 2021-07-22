bdabca60de08d1f6a230453f9da8dc310547b593
RobertoSuarez <electrosonix12@gmail.com>
2021-07-22 15:23:46 -0500

    Limpie el bind de generarCalendario
    En el boton generar, abre una modal para motrar una vista privia al reporte de generar Calendario

--- src/main/java/com/mycompanny/interfaces_mcp/bindGenerarCalendario.java
+++ src/main/java/com/mycompanny/interfaces_mcp/bindGenerarCalendario.java
@@ -6,14 +6,10 @@
 package com.mycompanny.interfaces_mcp;
 
 import Model.ManagerCalendario;
-import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.time.LocalDate;
-import javax.annotation.PostConstruct;
-import javax.inject.Named;
-import javax.faces.view.ViewScoped;
 
 import java.io.Serializable;
 import java.util.HashMap;
@@ -22,9 +18,6 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
@@ -92,118 +85,55 @@ public class bindGenerarCalendario implements Serializable {
         System.out.println("Iniciamos la class");
     }
     
-    public void mostrar() throws IOException, JRException {
-        System.out.println("Mostrar");
-        System.out.println(this.proveedor);
-        System.out.println(this.todosProveedores);
-        System.out.println(this.desde);
-        System.out.println(this.hasta);
-        System.out.println(this.sinfecha);
     
-        HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-       
-        response.setContentType("application/pdf");
-        
-        response.addHeader("Content-disposition", "attachment; filename=jsfReport.pdf");
-        
-        ManagerCalendario mc = new ManagerCalendario();
-        
-        Map<String, Object> parametros = new HashMap<String, Object>();
-        parametros.put("titulo", "Reporte desde java");
-        
-        try (ServletOutputStream outputStream = response.getOutputStream()) {
-            File filetext = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/Blank_A4.jasper"));
-            System.out.println("Se puede leer: " + filetext.canRead());
-            
-            JasperPrint jasperPrint = JasperFillManager.fillReport(
-                    filetext.getPath(), 
-                    parametros,
-                    new JRBeanCollectionDataSource(mc.getListCuotas())
-            );
-            
-            System.out.println("Ancho: " + jasperPrint.getPageWidth());
-           
-            JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
-            //JasperExportManager.exportReportToXmlStream(jasperPrint, outputStream);
-            
-            outputStream.flush();
-            outputStream.close();
-           
-        }
-        
-        FacesContext.getCurrentInstance().responseComplete();
-        
-        System.out.println("fin proccess");
-    }
     
-    public void mostrar2() throws IOException, JRException { 
-        
+    // Metodo funcional para exportar pdf
+    public void exportpdf() throws IOException, JRException { 
+    
         FacesContext fc = FacesContext.getCurrentInstance();
         ExternalContext ec = fc.getExternalContext();
-        
+    
+        // Cabecera de la respuesta.
         ec.responseReset();
         ec.setResponseContentType("application/pdf");
         ec.setResponseHeader("Content-disposition", "attachment; filename=hola.pdf");
         
+        // tomamos el stream para llenarlo con el pdf.
         try (OutputStream stream = ec.getResponseOutputStream()) {
             ManagerCalendario mc = new ManagerCalendario();
             
+            // Parametros para el reporte.
             Map<String, Object> parametros = new HashMap<String, Object>();
             parametros.put("titulo", "Reporte desde java");
             
+            // leemos la plantilla para el reporte.
+            File filetext = new File(FacesContext
+                    .getCurrentInstance()
+                    .getExternalContext()
+                    .getRealPath("/PlantillasReportes/Calendario.jasper"));
             
-            File filetext = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/Calendario.jasper"));
             
-            
+            // llenamos la plantilla con los datos.
             JasperPrint jasperPrint = JasperFillManager.fillReport(
                     filetext.getPath(),
                     parametros,
                     new JRBeanCollectionDataSource(mc.getListCuotas())
             );
             
+            // exportamos a pdf.
             JasperExportManager.exportReportToPdfStream(jasperPrint, stream);
             //JasperExportManager.exportReportToXmlStream(jasperPrint, outputStream);
             
             stream.flush();
             stream.close();
-        }
+}
 
-        
+        // enviamos la respuesta.
         fc.responseComplete();
         
         System.out.println("fin proccess");
     }
     
-    @PostConstruct
-    public void init() {
-        
-        
-    }
-   
     
-    public void exportarpdf(ActionEvent actionEvent) throws JRException, IOException {
-        Map<String, Object> parametros = new HashMap<String, Object>();
-        parametros.put("titulo", "Reporte desde java");
-        ManagerCalendario mc = new ManagerCalendario();
-        
-        File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/Blank_A4.jasper"));
-        System.out.println("path: " + jasper.getPath());
-        JasperPrint jasperfile = 
-                JasperFillManager.fillReport(
-                        jasper.getPath(), 
-                        parametros, 
-                        new JRBeanCollectionDataSource(mc.getListCuotas()));
-        HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-        response.addHeader("Content-Disposition", "attachment; filename=jsfReport.pdf");
-        
-        ServletOutputStream stream = response.getOutputStream();
-        
-        JasperExportManager.exportReportToPdfStream(jasperfile, stream);
-        
-        stream.flush();
-        stream.close();
-        
-        FacesContext.getCurrentInstance().responseComplete();
-    }
     
 }
