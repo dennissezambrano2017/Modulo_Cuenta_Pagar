0edcc0a5e092006f14f6b5c3c888e33440c1d7c6
eguarangao <69706207+eguarangao@users.noreply.github.com>
2021-08-10 00:00:31 -0500

    add condiciones and view proveedor new

---
+++ src/main/java/Model/Condiciones.java
@@ -0,0 +1,84 @@
+
+package Model;
+
+
+public class Condiciones {
+     int idCondiciones;
+     Double descuento;
+     int diasNeto;
+     int diasDescuento;
+     int cantDiasVencidos;
+     String descripcion;
+     Proveedor idProveedor;
+
+     public Condiciones(int idCondiciones, Double descuento, int diasNeto, int diasDescuento, int cantDiasVencidos, String descripcion, Proveedor idProveedor) {
+          this.idCondiciones = idCondiciones;
+          this.descuento = descuento;
+          this.diasNeto = diasNeto;
+          this.diasDescuento = diasDescuento;
+          this.cantDiasVencidos = cantDiasVencidos;
+          this.descripcion = descripcion;
+          this.idProveedor = idProveedor;
+     }
+
+     
+     public Condiciones() {
+     }
+
+     public int getIdCondiciones() {
+          return idCondiciones;
+     }
+
+     public void setIdCondiciones(int idCondiciones) {
+          this.idCondiciones = idCondiciones;
+     }
+
+     public Double getDescuento() {
+          return descuento;
+     }
+
+     public void setDescuento(Double descuento) {
+          this.descuento = descuento;
+     }
+
+     public int getDiasNeto() {
+          return diasNeto;
+     }
+
+     public void setDiasNeto(int diasNeto) {
+          this.diasNeto = diasNeto;
+     }
+
+     public int getDiasDescuento() {
+          return diasDescuento;
+     }
+
+     public void setDiasDescuento(int diasDescuento) {
+          this.diasDescuento = diasDescuento;
+     }
+
+     public int getCantDiasVencidos() {
+          return cantDiasVencidos;
+     }
+
+     public void setCantDiasVencidos(int cantDiasVencidos) {
+          this.cantDiasVencidos = cantDiasVencidos;
+     }
+
+     public String getDescripcion() {
+          return descripcion;
+     }
+
+     public void setDescripcion(String descripcion) {
+          this.descripcion = descripcion;
+     }
+
+     public Proveedor getidProveedor() {
+          return idProveedor;
+     }
+
+     public void setidProveedor(Proveedor idProveedor) {
+          this.idProveedor = idProveedor;
+     }
+     
+}
