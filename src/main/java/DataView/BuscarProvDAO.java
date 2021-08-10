99b4ae8111661e179f99e39b50ab5b23382afb2e
dennissezambrano2017 <66701370+dennissezambrano2017@users.noreply.github.com>
2021-08-09 22:29:03 -0500

    Función de Diana

--- src/main/java/DataView/BuscarProvDAO.java
+++ src/main/java/DataView/BuscarProvDAO.java
@@ -12,7 +12,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /**
  *
@@ -24,12 +23,11 @@ public class BuscarProvDAO {
     private Factura factura;
     private ResultSet result;
     private List<Proveedor> listaProveedor;
-    private List<Factura> listaFactura;
+    private List<Factura> listaFactura= new ArrayList<>();
 
     public BuscarProvDAO() {
         conexion = new Conexion();
         listaProveedor = new ArrayList<>();
-        listaFactura= new ArrayList<>();
     }
 
     public BuscarProvDAO(Proveedor proveedor) {
@@ -64,4 +62,48 @@ public class BuscarProvDAO {
         return listaProveedor;
     }
 
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    public List<Proveedor> getListaProveedor() {
+        return listaProveedor;
+    }
+
+    public void setListaProveedor(List<Proveedor> listaProveedor) {
+        this.listaProveedor = listaProveedor;
+    }
+    //Diana:Utiliza esta función 
+    public String Buscar(String nfactura) {
+        if (conexion.isEstado()) {
+            try {
+                String sentencia = "SELECT * from proveedor where idproveedor ="
+                        + "(SELECT  idproveedor from factura where nfactura = '"
+                        + factura.getNfactura() + "')";
+                result = conexion.ejecutarConsulta(sentencia);
+                while (result.next()) {
+                    listaProveedor.add(new Proveedor(result.getString("codigo"),
+                            result.getString("ruc"), result.getString("nombre")));
+                }
+                result.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage() + " error en conectarse");
+            } finally {
+                conexion.cerrarConexion();
+            }
+        }
+        return listaProveedor.get(0).getRuc();
+    }
 }
