b15bab94e9edc84a8670d9094e0f2f87ffb48351
RobertoSuarez <electrosonix12@gmail.com>
2021-08-15 14:42:35 +0200

    importación de paquetes

--- src/main/java/Model/Factura.java
+++ src/main/java/Model/Factura.java
@@ -9,6 +9,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
