a80cac010ed31743847cbddc55d89909c62b8e48
DianaAvilesCh <diana.aviles2017@uteq.edu.ec>
2021-08-24 01:02:13 -0500

    GET and PUT

--- src/main/java/com/servicio/ws/FacturaResource.java
+++ src/main/java/com/servicio/ws/FacturaResource.java
@@ -9,9 +9,12 @@ import Controller.FacturaManagedBean;
 import Model.Factura;
 import java.util.List;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -34,5 +37,20 @@ public class FacturaResource {
         @Produces(MediaType.APPLICATION_JSON)
         public Factura addFactura(Factura factura){
             return servicio.insert(factura);
+        }
+        
+        @DELETE
+        @Path("/{nfactura}")
+        public void dhFactura(@PathParam("nfactura")String nfactura){
+            servicio.dhFactura(nfactura);
+        }
+        
+        @PUT
+        @Path("/{nfactura}")
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        public Factura editFactura(@PathParam("nfactura")String nfactura,Factura factura){
+            factura.setNfactura(nfactura);
+            return servicio.editarfactura(factura);
         }
 }
